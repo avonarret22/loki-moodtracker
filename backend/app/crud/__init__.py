a7c69b617cf1eff42056474d7f803b2b23670d44
avonarret22 <148813906+avonarret22@users.noreply.github.com>
2025-10-27 14:32:46 -0300

    fix: export get_or_create_usuario in crud __init__

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -1,5 +1,5 @@
 # Import all CRUD functions here
-from app.crud.mood import get_usuario, get_usuario_by_telefono, get_usuarios, create_usuario
+from app.crud.mood import get_usuario, get_usuario_by_telefono, get_usuarios, create_usuario, get_or_create_usuario
 from app.crud.mood import get_estado_animo, get_estados_animo_by_usuario, create_estado_animo
 from app.crud.mood import (
     get_habito, get_habitos_by_usuario, create_habito, update_habito, delete_habito,
